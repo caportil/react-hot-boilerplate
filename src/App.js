7e84b57ef57d9761a04fb45f819bbb993a56bbdf
caportil <caportil@gmail.com>
2017-01-13 16:58:13 -0800

    Update port to 8000 and create sample form w out refresh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class App extends Component {
+	constructor(props) {
+		super(props);
+		let self = this;
+		this.state = {
+			//enter state properties here
+		}
+	}
+
   render() {
     return (
-      <h1>Hello, world.</h1>
+    	
+
+    	<div onSubmit={(event) => {
+    		event.preventDefault();
+    		console.log('CommentText submitted! Body:' + document.getElementsByName('CommentText')[0].value);
+    	}}>
+      	<h1>Hello, world.</h1>
+      	<form>Comment: <input name="CommentText" />
+      	<button>Submit</button>
+      	</form>
+      </div>
+
+
     );
   }
 }
+
+
+export default App;
